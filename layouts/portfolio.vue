2a76fa79cd36b961018d1070bfd60c5b0b136601
Calss0t <pol.texido.b@gmail.com>
2023-04-23 12:00:39 +0900

    first commit

---
+++ layouts/portfolio.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/"> Nuxt Dojo</NuxtLink>
+                
+            </nav>
+        </header>
+
+        <div>
+            <slot />
+        </div>
+
+        <footer>
+            <ul>
+                    <li>
+                        <NuxtLink to="/"> Home </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about"> About </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/portfolio"> Portfolio </NuxtLink>
+                    </li>
+                </ul>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+    /* body {
+        background-color: coral;
+    } */
+    .router-link-exact-active {
+        color: green
+    }
+</style>
---
+++ pages/portfolio.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Portfolio</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minima, repellendus deleniti nam neque quisquam
+            illum aliquam omnis nulla explicabo.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minima, repellendus deleniti nam neque quisquam
+            illum aliquam omnis nulla explicabo.</p>
+
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'portfolio'
+})
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px;
+}
+</style>
